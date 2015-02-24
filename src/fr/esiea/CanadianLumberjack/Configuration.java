760bf67d01a3de9d5c99add482c03b7b171f786b
MrWhiteFox <mr.whitefox@outlook.com>
2015-02-24 17:01:03 +0100

    Création de la structure de la classe Configuration

---
+++ src/fr/esiea/CanadianLumberjack/Configuration.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package fr.esiea.CanadianLumberjack;
+
+import java.io.File;
+
+/**
+ * Classe de configuration
+ *
+ */
+public class Configuration {
+
+	/**
+	 * Constructeur avec les valeurs par défaut.
+	 * Les paramètres par défaut sont :
+	 * Pour toutes les classes, errorlevel <code>ERROR</code>, cible <code>CibleConsole</code>
+	 *  
+	 */
+	public Configuration(){
+		
+	}
+	
+	/**
+	 * Utilise la configuration contenue dans le fichier properties passé en paramètre
+	 * 
+	 * DOCUMENTATION DU FICHIER PROPERTIES :
+	 * à faire
+	 * 
+	 * @param config
+	 * @param classe
+	 */
+	public Configuration(File configFile)/* throws FileException, BadConfigException */{
+		/* A DEFINIR */
+		
+		/* le fileexception est à redéfinir (il faut jeter une exception s'il y a un souci avec le fichier)
+		 * et le badconfigexceptin est à créer */
+		
+		 
+	}
+	
+	/**
+	 * Ajoute une cible à une classe.
+	 * L'utiliser pour la première fois écrase la valeur par défaut.
+	 * 
+	 * @param classe
+	 * @param cilbe
+	 */
+	public void ajouterCible(Class classe, Cible cilbe){
+		/* A DEFINIR */
+	}
+}
