ee01b45b340a136362acd45299e3923448bf6cb4
MrWhiteFox <mr.whitefox@outlook.com>
2015-02-24 17:07:36 +0100

    Ajout de la structure de l'interface Formateur

---
+++ src/fr/esiea/CanadianLumberjack/Formateur.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package fr.esiea.CanadianLumberjack;
+
+/**
+ * Formateur de message.
+ *
+ */
+public abstract class Formateur {
+
+	/**
+	 * Formate un message de type <code>ERROR</code>.
+	 * @param message
+	 * @return
+	 */
+	public abstract String formatError(String message);
+	
+	/**
+	 * Formate un message de type <code>INFO</code>.
+	 * @param message
+	 * @return
+	 */
+	public abstract String formatInfo(String message);
+	
+	/**
+	 * Formate un message de type <code>DEBUG</code>.
+	 * @param message
+	 * @return
+	 */
+	public abstract String formatDebug(String message);
+}
