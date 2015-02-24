fcbfe8b2f3bcd9c5eaa1dda476b20fb0fb5153b0
MrWhiteFox <mr.whitefox@outlook.com>
2015-02-24 17:00:04 +0100

    La config avec le fichier est dans config et pas le factory.

--- src/fr/esiea/CanadianLumberjack/LoggerFactory.java
+++ src/fr/esiea/CanadianLumberjack/LoggerFactory.java
@@ -20,19 +20,7 @@ public class LoggerFactory {
 		
 	}
 	
-	/**
-	 * Instancie un LoggerFactory avec la configuration contenue dans le fichier properties passé en paramètre
-	 * @param config
-	 * @param classe
-	 */
-	public LoggerFactory(File configFile)/* throws FileException, BadConfigException */{
-		/* A DEFINIR */
-		
-		/* le fileexception est à redéfinir (il faut jeter une exception s'il y a un souci avec le fichier)
-		 * et le badconfigexceptin est à créer */
-		
-		 
-	}
+	
 	
 	
 	
