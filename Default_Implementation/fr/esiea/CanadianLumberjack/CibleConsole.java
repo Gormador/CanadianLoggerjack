297e6c5ef1213582c0ee777aeb01881cee12b7be
Fourbartemis <marie.bougeret@gmail.com>
2015-02-26 15:02:17 +0100

    Ajout de la CibleRotatif; il faut encore voir si il est necessaire d'enregistrer en dur, le dernier fichier non plein ou non

--- Default_Implementation/fr/esiea/CanadianLumberjack/CibleConsole.java
+++ Default_Implementation/fr/esiea/CanadianLumberjack/CibleConsole.java
@@ -3,6 +3,10 @@
  */
 package fr.esiea.CanadianLumberjack;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Affiche les messages en console
  *
@@ -12,8 +16,43 @@ public class CibleConsole implements Cible {
 	/* (non-Javadoc)
 	 * @see fr.esiea.CanadianLumberjack.Cible#record(java.lang.String)
 	 */
-	@Override
-	public void record(String message) {
+	
+	private File file;
+	private String name;
+	private int number;
+	
+	
+	CibleConsole(){
+		name = "fichierRotatif";
+		file = new File(name + ".txt");
+		number = 1;
 	}
-
+	
+	@Override
+	public void record(String message) {		
+		int tailleFichier = message.getBytes().length + (int)file.length();
+				
+		try {
+			FileWriter fileWriter = new FileWriter(file);
+			
+			if(tailleFichier < 100){
+				fileWriter.write(message);
+				fileWriter.close();
+			}
+			else if (tailleFichier == 100){
+				fileWriter.write(message);
+				fileWriter.close();
+				file = new File("fichierRotatif" + number + ".txt");
+				number ++;
+			}
+			else{
+				file = new File("fichierRotatif" + number + ".txt");
+				number ++;
+				fileWriter.write(message);
+				fileWriter.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
