d81ebac2e3990fd5027d8404db6c97d0b3339dd5
MrWhiteFox <mr.whitefox@outlook.com>
2015-02-26 22:54:08 +0100

    Implem dans config

--- Framework/fr/esiea/CanadianLumberjack/Configuration.java
+++ Framework/fr/esiea/CanadianLumberjack/Configuration.java
@@ -22,6 +22,10 @@ public class Configuration {
 	
 	@SuppressWarnings("rawtypes")
 	private HashMap<Class, LinkedList<Target>> targets;
+	@SuppressWarnings("rawtypes")
+	private HashMap<Class, ErrorLevel> errLevels;
+	@SuppressWarnings("rawtypes")
+	private HashMap<Class, Formator> layouts;
 	
 	/**
 	 * Constructor with default parameters.
@@ -88,6 +92,34 @@ public class Configuration {
 			l.add(target);
 			targets.put(sClass, l);
 		}
-			/* A DEFINIR */
+			/* TODO */
 	}
+	
+	/**
+	 * Set the error level for this class.
+	 * <br> If an error level was already specified, it will be overwritten by the new value.
+	 * 
+	 * @param sClass The specified class
+	 * @param errorLevel The errorLevel
+	 * @see ErrorLevel
+	 */
+	@SuppressWarnings("rawtypes")
+	public void setLevel(Class sClass, ErrorLevel errorLevel){
+		errLevels.put(sClass, errorLevel);	
+	}
+	
+	/**
+	 * Set the layout for this class.
+	 * <br> If a layout was already specified, it will be overwritten by the new value.
+	 * 
+	 * @param sClass The specified class.
+	 * @param layout The layout that will be applied on all targets.
+	 */
+	@SuppressWarnings("rawtypes")
+	public void setLevel(Class sClass, Formator layout){
+		layouts.put(sClass, layout);	
+	}
+	
+	
+	
 }
