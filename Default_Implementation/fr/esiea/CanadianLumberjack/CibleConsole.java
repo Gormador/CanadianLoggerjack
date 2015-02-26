694366d971308a6b302e9a3f5eb43a8c43fe811c
Fourbartemis <marie.bougeret@gmail.com>
2015-02-26 15:22:09 +0100

    Correction de l'inversion qu'il y a eut dans le précédent commit (le CibleRotatif avait été créer dans le CibleConsole)

--- Default_Implementation/fr/esiea/CanadianLumberjack/CibleConsole.java
+++ Default_Implementation/fr/esiea/CanadianLumberjack/CibleConsole.java
@@ -17,42 +17,8 @@ public class CibleConsole implements Cible {
 	 * @see fr.esiea.CanadianLumberjack.Cible#record(java.lang.String)
 	 */
 	
-	private File file;
-	private String name;
-	private int number;
-	
-	
-	CibleConsole(){
-		name = "fichierRotatif";
-		file = new File(name + ".txt");
-		number = 1;
-	}
-	
 	@Override
 	public void record(String message) {		
-		int tailleFichier = message.getBytes().length + (int)file.length();
-				
-		try {
-			FileWriter fileWriter = new FileWriter(file);
-			
-			if(tailleFichier < 100){
-				fileWriter.write(message);
-				fileWriter.close();
-			}
-			else if (tailleFichier == 100){
-				fileWriter.write(message);
-				fileWriter.close();
-				file = new File("fichierRotatif" + number + ".txt");
-				number ++;
-			}
-			else{
-				file = new File("fichierRotatif" + number + ".txt");
-				number ++;
-				fileWriter.write(message);
-				fileWriter.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		System.out.println(message);
 	}
 }
--- Default_Implementation/fr/esiea/CanadianLumberjack/CibleFichierRotatif.java
+++ Default_Implementation/fr/esiea/CanadianLumberjack/CibleFichierRotatif.java
@@ -3,6 +3,10 @@
  */
 package fr.esiea.CanadianLumberjack;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Enregistre les messages dans un fichier rotatif.
  * Lorsque le fichier atteint une certaine taille, un nouveau fichier est automatiquement créé.
@@ -10,10 +14,44 @@ package fr.esiea.CanadianLumberjack;
  */
 public class CibleFichierRotatif implements Cible{
 
+
+	private File file;
+	private String name;
+	private int number;
+	
+	
+	CibleFichierRotatif(){
+		name = "fichierRotatif";
+		file = new File(name + ".txt");
+		number = 1;
+	}
+	
 	@Override
-	public void record(String message) {
-		// TODO Auto-generated method stub
-		
+	public void record(String message) {		
+		int tailleFichier = message.getBytes().length + (int)file.length();
+				
+		try {
+			FileWriter fileWriter = new FileWriter(file);
+			
+			if(tailleFichier < 100){
+				fileWriter.write(message);
+				fileWriter.close();
+			}
+			else if (tailleFichier == 100){
+				fileWriter.write(message);
+				fileWriter.close();
+				file = new File("fichierRotatif" + number + ".txt");
+				number ++;
+			}
+			else{
+				file = new File("fichierRotatif" + number + ".txt");
+				number ++;
+				fileWriter.write(message);
+				fileWriter.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
