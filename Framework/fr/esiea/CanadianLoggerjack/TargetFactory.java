bd03a70e7328f761a34281b426f6d952f908f6b4
MrWhiteFox <mr.whitefox@outlook.com>
2015-02-27 02:25:07 +0100

    add

--- Framework/fr/esiea/CanadianLoggerjack/TargetFactory.java
+++ Framework/fr/esiea/CanadianLoggerjack/TargetFactory.java
@@ -21,4 +21,14 @@ abstract class TargetFactory {
 	 */
 	public abstract Target getTarget(String props) throws InvalidConfigurationException;
 	
+	/**
+	 * This method sets the properties for the target when it is configured from a properties file.
+	 * <br>This method is call when no parameters are set in the config file.
+	 * <br>If your class cannot be instantiated without parameter, please always throw an <code>InvalidConfigurationException</code>.
+	 * 
+	 * @param props A properties objects containing all the properties specified in the config file.
+	 * @throws InvalidConfigurationException
+	 */
+	public abstract Target getTarget() throws InvalidConfigurationException;
+	
 }
